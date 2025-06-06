a334ba413ac1377be4a88b31321bd3a5cc69aa92
JonasHarder488 <jonas.harder@strandhotel-usedom.info>
2025-06-06 15:25:34 +0200

    first direction of nonempty_set_distset proven and comments added

--- FormalizationSoSe25/Projects/project_sample.lean
+++ FormalizationSoSe25/Projects/project_sample.lean
@@ -1,6 +1,8 @@
 import Mathlib.Tactic
 
---universe u
+/- This section defines a product between subsets of X×X (for X some set) as well as an operation for taking inverses
+  and the diagonal set as a neutral element. These will be needed in order to define coarse spaces.
+-/
 
 section set_operations
   def SetInv {X : Type*} (sub : Set (X × X)) : Set (X × X) := by
@@ -18,6 +20,8 @@ section set_operations
 
 end set_operations
 
+/- Now we define the class CoarseSpace analogously to a Topology. A coarse space consists of a set X and a coarse structure
+ε ⊆ P(X×X) ("controlled sets"), where ε is closed under union, inverse and product as well as contains the diagonal.-/
 
 class CoarseSpace (X : Type*) where
   IsControlled : Set (X × X) → Prop
@@ -26,13 +30,15 @@ class CoarseSpace (X : Type*) where
   IsControlled_inv : ∀ s : Set (X×X), IsControlled s → IsControlled (SetInv s)
   IsControlled_prod : ∀ s t : Set (X × X), IsControlled s → IsControlled t → IsControlled (SetProd s t)
 
-#print MetricSpace
-#check Real.exists_isLUB
-#check dist (3: ℝ) (5: ℝ)
 
 
--- proof that any metric space is coarse
+/- In the following, we want to establish that every MetricSpace is coarse by showing that an arbitrary
+MetricSpace is an instance of a CoarseSpace. This holds, if we define S ⊆ X×X controlled ↔ sup{d(x,y)| (x,y) ∈ S} < ∞.
+We start with a section for auxiliary lemmas and definitions.
+-/
 
+section lemmas_defs_for_metric_coarse
+-- Coordinate projections π₁, π₂ for elements x ∈ s ⊆ X×X
 
 def π₁ {X : Type*} {s : Set (X×X)} (x:X× X) (h : x∈ s) : X := by
   let ⟨ x₁, x₂⟩ := x
@@ -42,7 +48,11 @@ def π₂ {X : Type*} {s : Set (X×X)} (x:X× X) (h : x∈ s) : X := by
   let ⟨ x₁, x₂⟩ := x
   exact x₂
 
+-- The dist_set of s ⊆ X×X is defined as {d(x,y)| (x,y) ∈ S} ⊆ ℝ
+
 def dist_set {X : Type*} [MetricSpace X] (s : Set (X×X)) : Set ℝ := { dist (π₁ x h) (π₂ x h) | (x:X×X) (h:x∈ s) }
+
+-- Lemma for showing that the dist_set of a nonempty set is nonempty
 
 lemma nonempty_set_distset {X : Type*} [MetricSpace X] (s : Set (X×X)) : s.Nonempty → (dist_set s).Nonempty := by
   intro non_s
@@ -54,6 +64,7 @@ lemma nonempty_set_distset {X : Type*} [MetricSpace X] (s : Set (X×X)) : s.None
 
 def exists_diam {X : Type*} [MetricSpace X] (s : Set (X×X)) : Prop := (dist_set s).Nonempty ∧ BddAbove (dist_set s)
 
+end lemmas_defs_for_metric_coarse
 
 instance (X : Type*) [MetricSpace X] : CoarseSpace X where
   IsControlled := exists_diam
