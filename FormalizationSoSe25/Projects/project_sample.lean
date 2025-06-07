66dd8f5b43c5bcc914e196c3570fa4e79e4a132e
JonasHarder488 <jonas.harder@strandhotel-usedom.info>
2025-06-07 15:47:27 +0200

    completed lemma nonempty_set_distset, proof of first direction of IsControlled_union for metric space

--- FormalizationSoSe25/Projects/project_sample.lean
+++ FormalizationSoSe25/Projects/project_sample.lean
@@ -33,7 +33,7 @@ class CoarseSpace (X : Type*) where
 
 
 /- In the following, we want to establish that every MetricSpace is coarse by showing that an arbitrary
-MetricSpace is an instance of a CoarseSpace. This holds, if we define S ⊆ X×X controlled ↔ sup{d(x,y)| (x,y) ∈ S} < ∞.
+MetricSpace is an instance of a CoarseSpace. This holds, if we define s ⊆ X×X controlled ↔ diam(s) := sup{d(x,y)| (x,y) ∈ S} < ∞.
 We start with a section for auxiliary lemmas and definitions.
 -/
 
@@ -50,35 +50,65 @@ def π₂ {X : Type*} {s : Set (X×X)} (x:X× X) (h : x∈ s) : X := by
 
 -- The dist_set of s ⊆ X×X is defined as {d(x,y)| (x,y) ∈ S} ⊆ ℝ
 
+@[simp]
 def dist_set {X : Type*} [MetricSpace X] (s : Set (X×X)) : Set ℝ := { dist (π₁ x h) (π₂ x h) | (x:X×X) (h:x∈ s) }
 
--- Lemma for showing that the dist_set of a nonempty set is nonempty
+-- Lemma for showing that the dist_set of a nonempty set is nonempty and vice versa (very useful for later)
 
-lemma nonempty_set_distset {X : Type*} [MetricSpace X] (s : Set (X×X)) : s.Nonempty → (dist_set s).Nonempty := by
+@[simp]
+lemma nonempty_set_distset {X : Type*} [MetricSpace X] (s : Set (X×X)) : s.Nonempty ↔ (dist_set s).Nonempty := by
+  constructor
   intro non_s
-  have (x : X× X) (h : x ∈ s):= by
+  have xh : ∃ x : X× X, x ∈ s := by
     exact non_s
-  sorry
+  let ⟨x,h⟩:= xh
+  unfold dist_set
+  use dist (π₁ x h) (π₂ x h)
+  tauto
+  --
+  intro non_ds
+  have rh : ∃ r : ℝ, r ∈ (dist_set s) := by
+    exact non_ds
+  let ⟨r,h⟩:= rh
+  simp at h
+  let ⟨h₁,h₂,h₃,h₄⟩:= h
+  use (⟨h₁,h₂⟩)
 
 
+-- Definition for the conditions of the existence of diam(s)
 
 def exists_diam {X : Type*} [MetricSpace X] (s : Set (X×X)) : Prop := (dist_set s).Nonempty ∧ BddAbove (dist_set s)
 
 end lemmas_defs_for_metric_coarse
+
+-- Proof that every MetricSpace is a CoarseSpace
 
 instance (X : Type*) [MetricSpace X] : CoarseSpace X where
   IsControlled := exists_diam
   IsControlled_union := by
     rintro s t ⟨non_s, bd_s⟩ ⟨non_t, bd_t⟩
     constructor
-    have hs : ∃ x : ℝ , x ∈ (dist_set s) := by
+    rw [<- nonempty_set_distset]
+    rw [<- nonempty_set_distset] at non_s
+    rw [<- nonempty_set_distset] at non_t
+    have xh :∃ x : X×X, x∈ s := by
       exact non_s
+    let ⟨x,h⟩:= xh
+    use x
+    tauto
+    --
+    unfold BddAbove
+    unfold upperBounds
+    have xsh : ∃ x: ℝ, ∀{a: ℝ}, a ∈ dist_set s → a ≤ x := by
+      exact bd_s
+    have ytg : ∃ y: ℝ, ∀{a: ℝ}, a ∈ dist_set t → a ≤ y := by
+      exact bd_t
+    let ⟨x,h⟩:= xsh
+    let ⟨y,g⟩:= ytg
+    /- Idea for proceeding: take s₁ ∈ s, t₁ ∈ t (by non_s, non_t),
+    set R:= dist s₁ t₁, and show that R + x + y is upper bound
+    (by triangle inequality)-/
     sorry
-
-
-
-
-
 
   IsControlled_diag := sorry
   IsControlled_inv := sorry
